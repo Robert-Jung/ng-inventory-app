844df92b08d42ae84b6991ee91ce86ecdeafce7f
Robert-Jung <rjinjung@gmail.com>
2017-07-13 02:58:48 -0700

    Creating new product.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  product: Product;
+
+  constructor() {
+  this.product = new Product(
+    'NICEHAT',
+    'A Nice Black Hat',
+    '/assests/images/products/black-hat.jpg',
+    ['Men', 'Accessories', 'Hats'],
+    29.99);
+  }
 }
